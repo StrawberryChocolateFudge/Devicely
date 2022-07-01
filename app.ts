c035f9325af1814bd07c1ecba24626ae029f94b4
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-07-01 16:04:57 +0100

    added cors

--- app.ts
+++ app.ts
@@ -7,12 +7,10 @@ import express from "express";
 import fileUpload from "express-fileupload";
 import path from "path";
 import { fileURLToPath } from "url";
-const __filename = fileURLToPath(import.meta.url);
 
-const __dirname = path.dirname(__filename);
 import cookieParser from "cookie-parser";
 import logger from "morgan";
-
+import cors from "cors";
 import passport from "passport";
 import session from "express-session";
 
@@ -26,6 +24,10 @@ import indexRouter from "./routes/index.js";
 import authRouter from "./routes/auth.js";
 
 import * as ipfs from "ipfs-http-client";
+const __filename = fileURLToPath(import.meta.url);
+
+export const __dirname = path.dirname(__filename);
+
 //@ts-ignore
 const client = ipfs.create("/ip4/127.0.0.1/tcp/5001");
 const app = express();
@@ -35,7 +37,7 @@ app.locals.pluralize = pluralize;
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
+app.use(cors());
 app.use(logger("dev"));
 app.use(fileUpload());
 app.use(express.json());
@@ -69,5 +71,25 @@ app.use(function (err: any, req: any, res: any, next: any) {
   res.render("error");
 });
 
+const allowlist = ["http://ipfs.localhost:8080"];
+const corsOptionsDelegate = function (
+  req: Request,
+  callback: CallableFunction
+) {
+  let corsOptions;
+  //@ts-ignore
+  if (allowlist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
+export const url = process.env.URL;
+export const assetsLink = process.env.ASSETSLINK;
+export const escrowURL = process.env.ESCROWURL;
+export const contractAddress = process.env.CONTRACTADDRESS;
+export const rpcURL = process.env.RPCURL;
+
 export default app;
-export { client };
+export { client, corsOptionsDelegate };
